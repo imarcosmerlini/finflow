34522beaf618f4f9f1e28760b4196296ebb3e4e4
Marcos Merlini <merlinimarcos@gmail.com>
2024-09-30 02:30:00 -0300

    refactor: removed example controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
@@ -22,7 +20,6 @@ import { ErrorsService } from './errors/errors.service';
     AuthTransactionsModule,
     NotificationsModule,
   ],
-  controllers: [AppController],
-  providers: [AppService, ErrorsService],
+  providers: [ErrorsService],
 })
 export class AppModule {}
--- src/wallets/wallets.module.ts
+++ src/wallets/wallets.module.ts
@@ -3,9 +3,10 @@ import { WalletsService } from './wallets.service';
 import { PrismaService } from '../prisma.service';
 import { WalletsController } from './wallets.controller';
 import { UsersService } from '../users/users.service';
+import { ErrorsService } from '../errors/errors.service';
 
 @Module({
-  providers: [WalletsService, PrismaService, UsersService],
+  providers: [WalletsService, PrismaService, UsersService, ErrorsService],
   exports: [WalletsService],
   controllers: [WalletsController],
 })
